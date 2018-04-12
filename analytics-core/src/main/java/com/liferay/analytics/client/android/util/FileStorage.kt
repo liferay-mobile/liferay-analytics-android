22d608e5cc3ef8dfe7be22af7372b3f262cae73e
Igor Matos <igor.matos@liferay.com>
2018-04-12 17:47:29 -0300

    LPS-79605 FileDAO in Kotlin

---
+++ analytics-core/src/main/java/com/liferay/analytics/client/android/util/FileStorage.kt
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.client.android.util
+
+import android.content.Context
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
+/**
+ * @author Igor Matos
+ */
+class FileStorage(private val _context: Context) {
+
+    @Throws(IOException::class)
+    fun getStringByKey(key: String): String {
+        val inputStream = _context.openFileInput(key)
+
+        val inputStreamReader = InputStreamReader(
+                inputStream)
+
+        val bufferedReader = BufferedReader(inputStreamReader)
+
+        val stringBuilder = StringBuilder()
+
+        bufferedReader.lineSequence().forEach {
+            stringBuilder.append(it)
+        }
+
+        return stringBuilder.toString()
+    }
+
+    @Throws(IOException::class)
+    fun saveStringToKey(key: String, value: String) {
+        val outputStream = _context.openFileOutput(
+                key, Context.MODE_PRIVATE)
+
+        outputStream.write(value.toByteArray())
+        outputStream.close()
+    }
+
+}
