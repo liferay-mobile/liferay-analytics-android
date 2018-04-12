2087528a92ec64d5cc5c8dcffa2a30d6b0623327
Igor Matos <igor.matos@liferay.com>
2018-04-12 17:52:22 -0300

    LPS-79603 JSONParser

---
+++ analytics-core/src/main/java/com/liferay/analytics/client/android/util/JSONParser.kt
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.client.android.util
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonSyntaxException
+
+import com.liferay.analytics.model.AnalyticsEventsMessage
+import com.liferay.analytics.model.IdentityContextMessage
+
+import java.lang.reflect.Type
+
+/**
+ * @author Igor Matos
+ * @author Allan Melo
+ */
+object JSONParser {
+
+    private const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
+
+    @Throws(JsonSyntaxException::class)
+    fun <T> fromJsonString(json: String, type: Type): T {
+
+        return gson().fromJson(json, type)
+    }
+
+    fun toJSON(element: Any): String {
+        return gson().toJson(element)
+    }
+
+    @Synchronized
+    internal fun gson(): Gson {
+        val gsonBuilder = GsonBuilder()
+
+        gsonBuilder.setDateFormat(DATE_FORMAT)
+
+        gsonBuilder.registerTypeAdapter(
+                AnalyticsEventsMessage::class.java,
+                AnalyticsEventsMessageSerializer())
+
+        gsonBuilder.registerTypeAdapter(
+                IdentityContextMessage::class.java,
+                IdentityContextMessageSerializer())
+
+        return gsonBuilder.create()
+    }
+
+}
