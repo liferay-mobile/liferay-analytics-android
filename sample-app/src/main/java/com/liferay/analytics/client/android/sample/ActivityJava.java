596ce1cd55a7e348904628c47c175e50bb3eda8c
Igor Matos <igor.matos@liferay.com>
2018-04-13 14:35:03 -0300

    LPS-79491 Sample app with Kotlin and Java

---
+++ sample-app/src/main/java/com/liferay/analytics/client/android/sample/ActivityJava.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.client.android.sample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.liferay.analytics.client.android.Analytics;
+import com.liferay.analytics.client.android.forms.FormContext;
+import com.liferay.analytics.client.android.forms.Forms;
+
+public class ActivityJava extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_java);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		final FormContext formContext = new FormContext("formform", "Form Title agora vai");
+
+		FloatingActionButton fab = findViewById(R.id.fab);
+
+		fab.setOnClickListener( new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Forms.formSubmitted(formContext);
+	
+				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+					.setAction("Action", null).show();
+			}
+		});
+	}
+
+}
