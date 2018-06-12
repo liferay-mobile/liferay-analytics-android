e0474496ab80a1a53e9fc7b78e082762a82f60b5
Igor Matos <igor.matos@liferay.com>
2018-06-12 16:29:50 -0300

    Source formatting

--- sample-app/src/main/java/com/liferay/analytics/android/sample/FormsActivityJava.java
+++ sample-app/src/main/java/com/liferay/analytics/android/sample/FormsActivityJava.java
@@ -22,7 +22,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.liferay.analytics.android.forms.FieldAttributes;
-import com.squareup.leakcanary.RefWatcher;
 import com.liferay.analytics.android.forms.FormAttributes;
 import com.liferay.analytics.android.forms.Forms;
 
